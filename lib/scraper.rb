3f553e9da3757ac55add7a7429076b66cc749f40
El-Potato-Slayer <leighchad.cl@gmail.com>
2020-10-12 14:32:24 +0200

    added the scraper class

---
+++ lib/scraper.rb
@@ -0,0 +1,12 @@
+require 'Nokogiri'
+require 'open-uri'
+
+class Scraper
+  attr_accessor :page, :doc
+
+  def initialize(page_url)
+    @page = URI.parse(page_url)
+    @doc = Nokogiri::HTML(@page.open)
+    puts @doc
+  end
+end
